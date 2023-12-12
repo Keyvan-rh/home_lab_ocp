5b09d458f34fdb6cd4d6891d767e76bf72d6ab21
keyvan pishevar <kpisheva@redhat.com>
2023-12-12 14:20:22 -0600

    test

---
+++ proxmox/prep-ignitions.sh
@@ -0,0 +1,12 @@
+cd ./ocp4
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+openshift-install create manifests
+cp manifests/cluster-scheduler-02-config.yml /tmp/.
+sed -i 's/mastersSchedulable: true/mastersSchedulable: false/g' manifests/cluster-scheduler-02-config.yml
+cat manifests/cluster-scheduler-02-config.yml
+openshift-install create ignition-configs
+cp *.ign /var/www/html/ignition/
+cd /var/www/html/ignition/
+chmod 777 *
+restorecon -vR /var/www/html/
